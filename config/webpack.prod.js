7641207b4ed5b75764a4c9eb4abb68efc5604569
Potykun <105052944+Potykun@users.noreply.github.com>
2023-11-29 20:24:30 +0100

    create 2 pages

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,53 +1,60 @@
-import fs from 'fs';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import FileIncludeWebpackPlugin from 'file-include-webpack-plugin-replace';
+import fs from "fs";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import FileIncludeWebpackPlugin from "file-include-webpack-plugin-replace";
 import CopyPlugin from "copy-webpack-plugin";
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import TerserPlugin from "terser-webpack-plugin";
 
-import * as path from 'path';
+import * as path from "path";
 
 const srcFolder = "src";
 const builFolder = "dist";
 const rootFolder = path.basename(path.resolve());
 
-let pugPages = fs.readdirSync(srcFolder).filter(fileName => fileName.endsWith('.pug'))
+let pugPages = fs
+	.readdirSync(srcFolder)
+	.filter((fileName) => fileName.endsWith(".pug"));
 let htmlPages = [];
 
 if (!pugPages.length) {
-	htmlPages = [new FileIncludeWebpackPlugin({
-		source: srcFolder,
-		destination: '../',
-		htmlBeautifyOptions: {
-			"indent-with-tabs": true,
-			'indent_size': 3
-		},
-		replace: [
-			{ regex: '../img', to: 'img' },
-			{ regex: '@img', to: 'img', },
-			{ regex: 'NEW_PROJECT_NAME', to: rootFolder }
-		],
-	})]
+	htmlPages = [
+		new FileIncludeWebpackPlugin({
+			source: srcFolder,
+			destination: "../",
+			htmlBeautifyOptions: {
+				"indent-with-tabs": true,
+				indent_size: 3,
+			},
+			replace: [
+				{ regex: "../img", to: "img" },
+				{ regex: "@img", to: "img" },
+				{ regex: "@html/", to: "" },
+				{ regex: "NEW_PROJECT_NAME", to: rootFolder },
+			],
+		}),
+	];
 }
 
 const paths = {
 	src: path.resolve(srcFolder),
-	build: path.resolve(builFolder)
-}
+	build: path.resolve(builFolder),
+};
 const config = {
 	mode: "production",
 	cache: {
-		type: 'filesystem'
+		type: "filesystem",
 	},
 	optimization: {
-		minimizer: [new TerserPlugin({
-			extractComments: false,
-		})],
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false,
+			}),
+		],
 	},
 	output: {
 		path: `${paths.build}`,
-		filename: 'app.min.js',
-		publicPath: '/',
+		filename: "app.min.js",
+		publicPath: "/",
 	},
 	module: {
 		rules: [
@@ -56,14 +63,15 @@ const config = {
 				use: [
 					MiniCssExtractPlugin.loader,
 					{
-						loader: 'string-replace-loader',
+						loader: "string-replace-loader",
 						options: {
-							search: '@img',
-							replace: '../img',
-							flags: 'g'
-						}
-					}, {
-						loader: 'css-loader',
+							search: "@img",
+							replace: "../img",
+							flags: "g",
+						},
+					},
+					{
+						loader: "css-loader",
 						options: {
 							importLoaders: 0,
 							sourceMap: false,
@@ -79,62 +87,72 @@ const config = {
 						},
 					},
 					{
-						loader: 'sass-loader',
+						loader: "sass-loader",
 						options: {
 							sassOptions: {
 								outputStyle: "expanded",
 							},
-						}
+						},
 					},
 				],
-			}, {
+			},
+			{
 				test: /\.pug$/,
 				use: [
 					{
-						loader: 'pug-loader'
-					}, {
-						loader: 'string-replace-loader',
+						loader: "pug-loader",
+					},
+					{
+						loader: "string-replace-loader",
 						options: {
-							search: '@img',
-							replace: 'img',
-							flags: 'g'
-						}
-					}
-				]
-			}
+							search: "@img",
+							replace: "img",
+							flags: "g",
+						},
+					},
+				],
+			},
 		],
 	},
 	plugins: [
 		...htmlPages,
-		...pugPages.map(pugPage => new HtmlWebpackPlugin({
-			minify: false,
-			template: `${srcFolder}/${pugPage}`,
-			filename: `../${pugPage.replace(/\.pug/, '.html')}`
-		})),
+		...pugPages.map(
+			(pugPage) =>
+				new HtmlWebpackPlugin({
+					minify: false,
+					template: `${srcFolder}/${pugPage}`,
+					filename: `../${pugPage.replace(/\.pug/, ".html")}`,
+				})
+		),
 		new MiniCssExtractPlugin({
-			filename: '../css/style.css',
+			filename: "../css/style.css",
 		}),
 		new CopyPlugin({
 			patterns: [
 				{
-					from: `${paths.src}/files`, to: `../files`,
-					noErrorOnMissing: true
-				}, {
-					from: `${paths.src}/php`, to: `../`,
-					noErrorOnMissing: true
-				}, {
-					from: `${paths.src}/favicon.ico`, to: `../`,
-					noErrorOnMissing: true
-				}
+					from: `${paths.src}/files`,
+					to: `../files`,
+					noErrorOnMissing: true,
+				},
+				{
+					from: `${paths.src}/php`,
+					to: `../`,
+					noErrorOnMissing: true,
+				},
+				{
+					from: `${paths.src}/favicon.ico`,
+					to: `../`,
+					noErrorOnMissing: true,
+				},
 			],
-		})
+		}),
 	],
 	resolve: {
 		alias: {
 			"@scss": `${paths.src}/scss`,
 			"@js": `${paths.src}/js`,
-			"@img": `${paths.src}/img`
+			"@img": `${paths.src}/img`,
 		},
 	},
-}
+};
 export default config;
